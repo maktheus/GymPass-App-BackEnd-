f2f73fae3a19e85fccfe430ad93761b326278b0f
Matheus Serrao Uchoa <maktheus@gmail.com>
2023-03-12 17:32:18 -0400

    first config prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-import fastify  from "fastify";
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
 
 export const app = fastify();
+
+prisma.$on("query", (e) => {
+    console.log(e);
+    }
+);
+
+app.get("/", async (request, reply) => {
+    return { hello: "world" };
+});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import { env } from "./env";
 import { app } from "./app";
 
-
-
-app.listen({
+app
+  .listen({
     port: env.PORT,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   })
   .then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
